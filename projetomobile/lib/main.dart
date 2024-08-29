1e74d74c8b3e9c2381dab98165a111cf31903a51
ViniMPostal <viniciusmppostal@gmail.com>
2024-08-28 21:20:33 -0300

    teste

--- projetomobile/lib/main.dart
+++ projetomobile/lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         // This is the theme of your application.
 
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Tela Inicial'),
