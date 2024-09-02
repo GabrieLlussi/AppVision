81e7cc7248afcc4e4f6a6e8eb951aecf31d044f5
Mfantasma <andrei.franzon@universo.univates.br>
2024-09-01 21:44:29 -0300

    Carrinho v2

--- projetomobile/lib/carrinho/carrinho.dart
+++ projetomobile/lib/carrinho/carrinho.dart
@@ -1,23 +1,68 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+import 'dart:convert';
 
-class Carrinho extends StatelessWidget {
+class Carrinho extends StatefulWidget {
+  @override
+  _CarrinhoState createState() => _CarrinhoState();
+}
+  
+  class _CarrinhoState extends State<Carrinho> {
   List<Map<String, dynamic>> produtos = [];
-
-    editarProduto(int index) {
-    
-  }
   
-  excluirProduto(int index) {
-    
-  }
-  
-  double calcularTotal() {
-    return produtos.fold(0, (soma, item) => soma + item['preco'] * item['quantidade']);
+  @override
+  void initState() {
+    super.initState();
+    _loadCart();
   }
 
-  void adicionarProduto() {
-    
+  Future<File> _getFilePath() async {
+    final directory = await getApplicationDocumentsDirectory();
+    return File('${directory.path}/carrinho.txt');
   }
+
+  Future<void> _loadCart() async {
+    final file = await _getFilePath();
+    if (await file.exists()) {
+      List<String> lines = await file.readAsLines();
+      setState(() {
+        produtos = lines.map((line) {
+          final Map<String, dynamic> produto = {};
+            line.split(', ').forEach((entry) {
+              final List<String> keyValue = entry.split(': ');
+              if (keyValue.length == 2) {
+                produto[keyValue[0]] = keyValue[1];
+              }
+            });
+          return produto;
+        }).toList();
+         print('Produtos carregados: ${produtos.length}');
+      });
+    }
+  }
+
+  void excluirProduto(int index) {
+    setState(() {
+      produtos.removeAt(index);
+      _saveCart(); // Atualiza o arquivo após excluir
+    });
+  }
+
+  /*double calcularTotal() {
+    return produtos.fold(0, (soma, item) {
+      double preco = double.tryParse(item['preco']) ?? 0;
+      int quantidade = int.tryParse(item['quantidade']) ?? 1;
+      return soma + preco * quantidade;
+    });
+  }
+*/
+  Future<void> _saveCart() async {
+    final file = await _getFilePath();
+    String produtosJson = produtos.map((produto) => produto.toString()).join('\n');
+    await file.writeAsString(produtosJson);
+  }
+
     @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,7 +74,9 @@ class Carrinho extends StatelessWidget {
       body: Column( 
         children: [
           Expanded(
-            child: ListView.builder(
+            child: produtos.isEmpty
+              ? Center(child: Text('Carrinho vazio'))
+            : ListView.builder(
               itemCount: produtos.length,
               itemBuilder: (context, index) {
                 final produto = produtos[index];
@@ -38,20 +85,22 @@ class Carrinho extends StatelessWidget {
                   child: ListTile(
                     leading: CircleAvatar(
                       backgroundColor: Colors.teal,
-                      child: Text(
+                      /*child: Text(
                         produto['quantidade'].toString(),
                         style: TextStyle(color: Colors.white),
-                      ),
+                      ),*/
                     ),
                     title: Text(produto['nome']),
-                    subtitle: Text('Preço: R\$ ${produto['preco'].toStringAsFixed(2)}'),
+                    subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Preço: R\$ ${produto['preco'].toStringAsFixed(2)}'),
+                        Text('Peso: ${produto['peso'].toStringAsFixed(2)} g'),
+                      ],
+                    ),
                     trailing: Row(
                       mainAxisSize: MainAxisSize.min,
                       children: [
-                        IconButton(
-                          icon: Icon(Icons.edit, color: Colors.blue),
-                          onPressed: () => editarProduto(index),
-                        ),
                         IconButton(
                           icon: Icon(Icons.delete, color: Colors.red),
                           onPressed: () => excluirProduto(index),
@@ -67,23 +116,12 @@ class Carrinho extends StatelessWidget {
             padding: const EdgeInsets.all(15.0),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
+              /*children: [
                 Text(
                   'Total: R\$ ${calcularTotal().toStringAsFixed(2)}',
                   style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                 ),
-                ElevatedButton(
-                  onPressed: adicionarProduto,
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.teal,
-                    padding: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
-                  ),
-                  child: Text(
-                    'Adicionar Item',
-                    style: TextStyle(fontSize: 18, color: Colors.black),
-                  ),
-                ),
-              ],
+              ],*/
             ),
           ),
           Padding(
--- projetomobile/lib/carrinho/carrinho_page.dart
+++ projetomobile/lib/carrinho/carrinho_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+import 'package:projetomobile/carrinho/carrinho.dart';
+import 'dart:convert';
 
 class CarrinhoPage extends StatefulWidget {
   @override
@@ -8,11 +12,13 @@ class CarrinhoPage extends StatefulWidget {
 
   class _CarrinhoPageState extends State<CarrinhoPage> {
   List<Map<String, dynamic>> produtos = [];
+  List<Map<String, dynamic>> produtosCarrinho = [];
 
   @override
   void initState() {
     super.initState();
-    _fetchProdutos();
+    _loadCart(); // Carrega o carrinho do arquivo
+    _fetchProdutos(); // Busca os produtos do Firestore
   }
 
   void _fetchProdutos() async {
@@ -34,9 +40,57 @@ class CarrinhoPage extends StatefulWidget {
     });
   }
 
-  void adicionarProduto() {
-    
+  // Obtém o diretório onde o arquivo será salvo
+  Future<String> _getFilePath() async {
+    Directory appDocumentsDirectory = await getApplicationDocumentsDirectory();
+    String filePath = '${appDocumentsDirectory.path}/carrinho.txt';
+    return filePath;
   }
+
+  // Salva os itens do carrinho no arquivo
+ Future<void> _saveCart() async {
+  String filePath = await _getFilePath();
+  File file = File(filePath);
+
+  String cartData = jsonEncode(produtosCarrinho); // Salvar como JSON
+
+  await file.writeAsString(cartData);
+}
+
+ // Carrega os itens do carrinho do arquivo
+  Future<void> _loadCart() async {
+    String filePath = await _getFilePath();
+    File file = File(filePath);
+
+    if (await file.exists()) {
+      String cartData = await file.readAsString();
+      setState(() {
+        produtos = cartData.split('\n').map((line) {
+          List<String> parts = line.split('|');
+          return {
+            'nome': parts[0],
+            'preco': double.parse(parts[1]),
+            'peso': double.parse(parts[2]),
+          };
+        }).toList();
+      });
+    }
+  } 
+
+  void adicionarProduto(Map<String, dynamic> produto) {
+    setState(() {
+      produtosCarrinho.add(produto);
+    });
+    _saveCart(); // Salva o carrinho após adicionar o produto
+    // Exibe um Snackbar para informar que o produto foi adicionado
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      content: Text('Produto adicionado ao carrinho'),
+      duration: Duration(seconds: 2),
+    ),
+  );
+  }
+
     @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -56,18 +110,14 @@ class CarrinhoPage extends StatefulWidget {
                   margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
                   child: ListTile(
                     leading: CircleAvatar(
-                      backgroundColor: Colors.teal,
-                      /*child: Text(
-                        produto['quantidade'].toString(),
-                        style: TextStyle(color: Colors.white),
-                      ),*/
+                      backgroundColor: Colors.teal,                   
                     ),
                     title: Text(produto['nome']),
                     subtitle: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                     Text('Preço: R\$ ${produto['preco'].toStringAsFixed(2)}'),
-                     Text('Peso: ${produto['peso'].toStringAsFixed(2)} g'),
+                        Text('Preço: R\$ ${produto['preco'].toStringAsFixed(2)}'),
+                        Text('Peso: ${produto['peso'].toStringAsFixed(2)} g'),
                       ],
                     ),
                     trailing: Row(
@@ -75,7 +125,11 @@ class CarrinhoPage extends StatefulWidget {
                       children: [
                         IconButton(
                           icon: Icon(Icons.add, color: Colors.blue),
-                          onPressed: () => adicionarProduto(),
+                          onPressed: () => adicionarProduto({
+                            'nome': produto['nome'],
+                            'preco': produto['preco'],
+                            'peso': produto['peso'],
+                          }),
                         ),
                       ],
                     ),
@@ -86,32 +140,14 @@ class CarrinhoPage extends StatefulWidget {
           ),
           Padding(
             padding: const EdgeInsets.all(15.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                /*Text(
-                  'Total: R\$ ${calcularTotal().toStringAsFixed(2)}',
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                ),*/
-                ElevatedButton(
-                  onPressed: adicionarProduto,
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.teal,
-                    padding: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
-                  ),
-                  child: Text(
-                    'Adicionar Item',
-                    style: TextStyle(fontSize: 18, color: Colors.black),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(15.0),
             child: ElevatedButton(
               onPressed: () {
                 // Lógica para finalizar a compra
+                // Navegar para a tela de carrinho de compras
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Carrinho()),
+                );
               },
               style: ElevatedButton.styleFrom(
                 backgroundColor: const Color.fromARGB(255, 153, 94, 248),
